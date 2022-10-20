3c3c0c5127190ee28181bc455a3405ab49dbd388
Noah Cain <noahcain.work@gmail.com>
2022-10-20 14:05:54 -0700

    final commit for makingDecisions2Lab.js

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -82,26 +82,26 @@ var score = 34
 
 //Code Here
 
-if (score < 60) {
-  console.log('F')
-} else if (score >= 60 && score <= 69) {
-  console.log('D')
-} else if (score >= 70 && score <=79) {
-  console.log('C')
-} else if (score >= 80 && score <= 89) {
-  console.log('B')
-} else if (score > 90) {
-  console.log('A')
-} else {
-  console.log('Please enter a valid number.')
-}
+// if (score < 60) {
+//   console.log('F')
+// } else if (score >= 60 && score <= 69) {
+//   console.log('D')
+// } else if (score >= 70 && score <=79) {
+//   console.log('C')
+// } else if (score >= 80 && score <= 89) {
+//   console.log('B')
+// } else if (score > 90) {
+//   console.log('A')
+// } else {
+//   console.log('Please enter a valid number.')
+// }
 
 ////////// Intermediate Problems //////////
 
 ////////// PROBLEM 6 //////////
 
 // Do not edit the code below.
-var myFavoriteNumbers = [4,8,12,16,20,24];
+var myFavoriteNumbers = [4,8,12,16,20,24,23,95,28];
 // Do not edit the code above.
 
 /*
@@ -111,14 +111,31 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 
 //Code Here
 
+let someNum = myFavoriteNumbers[4]
+// console.log(someNum)
 
 ////////// PROBLEM 7 //////////
 
-// Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
-// Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
+// Subscripting (accessing values using their index) an 
+// array can fail. What happens if you subscript to the 
+// 7th element, but there are not 7 elements in the array? 
+// Let's write some code to check for that. 
+// Use an if statement to check the length of the 
+// 'myFavoriteNumbers' array. If it's less than 7, log 
+// 'There are not enough elements in this array' to the 
+// console. If the length is more than 7, reassign the value 
+// of 'someNum' to the value of the 7th element in the array. 
+// (Hint: how can you make sure that your code works for exactly 
+//   7 elements? What index do you use to get the 7th element?)
 
 //Code Here
 
+// if (myFavoriteNumbers.length < 7) {
+//   console.log('There are not enough elements in this array.')
+// } else {
+//   someNum = myFavoriteNumbers[6]
+//   console.log(someNum)
+// }
 
 ////////// PROBLEM 8 //////////
 
@@ -126,10 +143,17 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Do not edit the code above.
 
-// Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
+// Use a for-loop to iterate through 'listOfNumbers', 
+// checking to see if each number is divisible by 3. If it 
+// is, console.log '{number} is divisible by 3.'
 
 //Code Here
 
+// for (let i = 0; i < listOfNumbers.length; i++) {
+//   if (listOfNumbers[i] % 3 === 0) {
+//     console.log(`${listOfNumbers[i]} is divisible by 3.`)
+//   }
+// }
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
@@ -137,11 +161,15 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 // Do not edit the code above.
 
 /*
-  Loop backwards, starting at the end of the 'letters' array. Console log every item in the array.
+  Loop backwards, starting at the end of the 'letters' array. 
+  Console log every item in the array.
 */
 
 //Code Here
 
+// for (let i = letters.length; i >= 0; i--) {
+//   console.log(letters[i])
+// }
 
 ////////// Advanced Problems //////////
 
@@ -152,23 +180,53 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 let letterGrade = 'B'
 // Do not edit the code above.
 
-/* Use a switch statement on 'letterGrade' and console.log the appropriate response.
+/* Use a switch statement on 'letterGrade' and console.log 
+the appropriate response.
 
 If A: "The student is doing excellently."
 If B: "The student is doing well."
 If C: "The student is doing alright."
 If D: "The student is not doing very well."
 If F: "The student is failing."
-If the letter grade is not one of the above letters, console.log 'Not an eligible grade.'
+If the letter grade is not one of the above letters, 
+console.log 'Not an eligible grade.'
 */
 
 //Code Here
+
+// switch(letterGrade) {
+//   case 'A':
+//     console.log('The student is doing excellently.')
+//     break;
+//   case 'B':
+//     console.log('The student is doing well.')
+//     break;
+//   case 'C':
+//     console.log('The student is doing alright.')
+//     break;
+//   case 'D':
+//     console.log('The student is not doing very well.')
+//     break;
+//   case 'F':
+//     console.log('The student is failing.')
+//     break;
+//   default:
+//     console.log('Not an eligible grade.')
+// }
+
 
 
 
 ////////// PROBLEM 11 //////////
 /* The famous FizzBuzz, Devmountain style!
-  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, console.log 'Dev'. If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
+  Create a for loop that iterates from 1 to 100. In this 
+  for loop, using some conditional logic, if the number 
+  your for loop is currently on is divisible by 3, 
+  console.log 'Dev'. If the number is divisible by 5, 
+  console.log 'mountain'. If the number is divisible 
+  by 5 & 3, console.log 'Devmountain'. If the number is 
+  not divisible by 5 or 3, console.log the number itself. 
+  Hint: Look up the modulo operator.
 
   Your output should look like:
   1
@@ -191,3 +249,15 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log('Devmountain')
+//   } else if (i % 5 === 0) {
+//     console.log('mountain')
+//   } else if (i % 3 === 0) {
+//     console.log('Dev')
+//   } else {
+//     console.log(i)
+//   }
+// }
